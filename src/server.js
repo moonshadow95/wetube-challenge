5b5cdeac09328d1c9adf6aceeb8950b795666897
moonshadow <last_enemy@naver.com>
2021-10-22 03:30:00 +0900

    Session Store (MongoStore)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import session from 'express-session';
+import MongoStore from 'connect-mongo';
 import rootRouter from './router/rootRouter';
 import userRouter from './router/userRouter';
 import videoRouter from './router/videoRouter';
@@ -8,12 +9,20 @@ import { localsMiddleware } from './middlewares';
 
 const app = express();
 const logger = morgan('dev');
-
 app.set('views', process.cwd() + '/src/views');
 app.set('view engine', 'pug');
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-app.use(session({ secret: 'secret!', resave: true, saveUninitialized: true }));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.DB_URL,
+    }),
+  })
+);
 app.use(localsMiddleware);
 
 app.use('/', rootRouter);
