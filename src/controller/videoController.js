607f6f78ee9949c5c4f7dc2c501adeb62b6e9b91
moonshadow <last_enemy@naver.com>
2021-10-21 14:54:49 +0900

    Watch page

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -5,7 +5,14 @@ export const home = async (req, res, next) => {
   return res.render('home', { pageTitle: 'Home', videos });
 };
 
-export const watch = (req, res) => {};
+export const watch = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render('404', { pageTitle: 'Video not found.' });
+  }
+  return res.render('watch', { pageTitle: video.title, video });
+};
 
 export const getEdit = (req, res, next) => {};
 
