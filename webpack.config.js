ac904f4711cccc372f7d5ad7016d5890feae0e10
moonshadow <last_enemy@naver.com>
2021-10-24 20:10:29 +0900

    SCSS Loader & MiniCssExtractPlugin Configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
-const BASE_JS = './src/client/js/';
 
 module.exports = {
-  entry: {
-    main: BASE_JS + 'main.js',
-  },
+  entry: './src/client/js/main.js',
   mode: 'development',
+  watch: true,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/styles.css',
+    }),
+  ],
   output: {
-    filename: 'js/[name].js',
+    filename: 'js/main.js',
     path: path.resolve(__dirname, 'assets'),
     clean: true,
   },
@@ -21,6 +25,10 @@ module.exports = {
             presets: [['@babel/preset-env', { targets: 'defaults' }]],
           },
         },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
